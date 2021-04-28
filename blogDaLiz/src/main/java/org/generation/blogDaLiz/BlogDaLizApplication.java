a7ec821afc7c391f7da740a67e88f11686ed237b
Liz St <lizstspsi@outlook.com>
2021-04-27 23:37:10 -0300

    spring boot exercicio2

---
+++ blogDaLiz/src/main/java/org/generation/blogDaLiz/BlogDaLizApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogDaLiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDaLizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDaLizApplication.class, args);
+	}
+
+}
---
+++ blogDaLiz/src/main/java/org/generation/blogDaLiz/repository/PostagemRepository.java
@@ -0,0 +1,14 @@
+package org.generation.blogDaLiz.repository;
+import java.util.List; 
+import org.generation.blogDaLiz.model.Postagem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface PostagemRepository extends JpaRepository <Postagem, Long> {
+
+	public List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+
+	
+}
